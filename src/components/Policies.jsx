8b04d86930dc647d183411c3d7bd51d561ae19dc
Stephen L <32889838+splawrence@users.noreply.github.com>
2025-10-16 16:18:44 -0500

    Remove testing note from editorial policy section

--- src/components/Policies.jsx
+++ src/components/Policies.jsx
@@ -21,7 +21,7 @@ const Policies = ({ onClose }) => {
               <h2>Editorial Policies</h2>
               <div className="policy-text">
                 <h3>Accuracy and Fact-Checking</h3>
-                <p>Stephen is testing this. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 
                 <h3>Independence and Objectivity</h3>
                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
